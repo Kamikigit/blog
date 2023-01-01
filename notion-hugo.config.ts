12d8d86f6b4b6dac474042c9343277f582701298
Kamikigit <40894402+Kamikigit@users.noreply.github.com>
2023-01-01 14:41:46 +0900

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://www.notion.so/Notion-DoIt-1de5b89e0b9147758c364249bb34486f',
         pages: [
             // {
             //     page_id: '<page_id>',
